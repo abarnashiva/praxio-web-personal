d6376b0eafac50ad48c4712c3ca66ea9c7c44c56
Pradeep Saravanan <79619944+developerpradeepofficial@users.noreply.github.com>
2023-10-30 16:21:21 +0530

    add services and card component

--- src/app/Services.tsx
+++ src/app/Services.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-
+import React from "react";
+import Card from "./common/Card";
 function Services() {
   return (
-    <section id="services" className="py-10">
+    <section id="services" className="py-10 h-[50rem] ">
       <h2 className="text-center text-2xl mb-8">Our Services</h2>
-      <div className="flex space-x-4 px-7">
-        <div className="flex-1 p-5 border">
-          <h3 className="text-xl mb-4">Service 1</h3>
-          <p>Description of Service 1.</p>
-        </div>
-        <div className="flex-1 p-5 border">
-          <h3 className="text-xl mb-4">Service 2</h3>
-          <p>Description of Service 2.</p>
-        </div>
-        <div className="flex-1 p-5 border">
-          <h3 className="text-xl mb-4">Service 3</h3>
-          <p>Description of Service 3.</p>
-        </div>
+      <div className="flex gap-5 justify-around">
+        <Card
+          header={"Software Development"}
+          shortDescription={"Caption or Thumbnail"}
+          content={
+            "Get room to focus on strategic business growth by cutting on development costs and outplay your rivals."
+          }
+        />
+
+        <Card
+          header={"AI and Machine Learning"}
+          shortDescription={"Caption or Thumbnail"}
+          content={
+            "Get room to focus on strategic business growth by cutting on development costs and outplay your rivals."
+          }
+        />
+        <Card
+          header={"IoT Application Development"}
+          shortDescription={"Caption or Thumbnail"}
+          content={
+            "Get room to focus on strategic business growth by cutting on development costs and outplay your rivals."
+          }
+        />
+        <Card
+          header={"Enterprise Application Development"}
+          shortDescription={"Caption or Thumbnail"}
+          content={
+            "Get room to focus on strategic business growth by cutting on development costs and outplay your rivals."
+          }
+        />
       </div>
     </section>
   );
---
+++ src/app/common/Card.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Arrow } from ".";
+interface CardProps {
+  header?: React.ReactNode;
+  shortDescription?: string;
+  content?: React.ReactNode;
+}
+
+export default function Card({ header, shortDescription, content }: CardProps) {
+  return (
+    <div>
+      <div className="relative card w-[22em]  max-w-4xl min-h-[8rem] p-8 bg-white shadow-md transition-all duration-350 ease-in-out cursor-pointer hover:shadow-lg">
+        <div className="content w-[18em] max-w-4xl">
+          <div className={`group flex items-center transition-all `}>
+            <div className="header">{header}</div>
+            <div className="icon ">
+              <Arrow fill="#ed1f24cc" />
+            </div>
+          </div>
+
+          <h6 className="text-primary font-semibold uppercase text-xs tracking-wider mb-0">
+            {shortDescription}
+          </h6>
+          <div className="hover_content overflow-hidden max-h-0 duration-500  transform translate-y-[1em] transition-all ease">
+            <div className="mt-4 text-gray-700 leading-relaxed">{content}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
