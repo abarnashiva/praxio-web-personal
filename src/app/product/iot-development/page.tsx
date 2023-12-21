face58522a4600383027f5047d80d5403c992057
maheswari3008 <maheswariravi3008@gmail.com>
2023-12-21 19:20:32 +0530

    add: home page images

--- src/app/product/iot-development/page.tsx
+++ src/app/product/iot-development/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Content from "./content";
-import CenteredBoxWithButton from "../../common/Box";
 
 export default function SmartCity() {
   return (
     <>
       <Content />
-      <CenteredBoxWithButton />
     </>
   );
 }
