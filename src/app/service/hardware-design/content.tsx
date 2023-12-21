7daa76bacc255a7393fa28b6a89b5b4267566bee
abarnashiva <abarnaabi57@gmail.com>
2023-12-21 18:51:54 +0530

    add: hardware design service page

---
+++ src/app/service/hardware-design/content.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+import Subheading from "../../components/SubHeading";
+import Paragraph from "../../components/Paragraph";
+import { Button } from "../../common/Button";
+import Heading from "../../components/Heading";
+import SubContent from "../../components/SubContent";
+import SemiHeading from "../../components/SemiHeading";
+import CardContent from "../../components/Cardcontent";
+import Numberunfill from "../../components/Numberunfill";
+import FlexContent from "../../components/FlexContent";
+
+export default function Content() {
+  return (
+    <div>
+      <Heading>Hardware Design & Prototyping</Heading>
+      <SubContent>
+        Turning innovative ideas into tangible reality through expert hardware
+        design and rapid prototyping solutions.
+      </SubContent>
+      <Subheading>
+        Innovative Designs, Seamless Prototypes: Transforming Ideas into Reality
+      </Subheading>
+      <Paragraph>
+        At PRAXIO, we specialize in cutting-edge hardware design and prototyping
+        services. Our dedicated team of experienced engineers and designers is
+        committed to bringing your innovative ideas to life. From concept
+        development and schematic design to PCB layout, fabrication, and
+        assembly, we offer comprehensive solutions tailored to your specific
+        needs.
+      </Paragraph>
+      <Paragraph>
+        Our state-of-the-art facilities and advanced technologies enable us to
+        create high-quality prototypes that meet industry standards and exceed
+        expectations. Whether you&#39;re a startup with a groundbreaking product
+        concept or an established business seeking to enhance your existing
+        hardware, our expertise ensures seamless transition from design to
+        physical prototype.
+      </Paragraph>
+      <Paragraph>
+        With a focus on precision, reliability, and innovation, we deliver
+        exceptional hardware solutions that pave the way for your success.
+        Partner with us and experience the journey of turning your vision into a
+        tangible, functional prototype. Explore the possibilities with PRAXIO.
+      </Paragraph>
+      <Button text={"Start Your Project "}></Button>
+      <SemiHeading>
+        Why choose PRAXIO for Hardware Design & Prototyping?
+      </SemiHeading>
+      <FlexContent>
+        <CardContent>
+          <Numberunfill>01</Numberunfill>
+          <SemiHeading>Conceptualization and Design </SemiHeading>
+          <Paragraph>
+            Our experts work closely with you to understand your vision. We
+            offer comprehensive conceptualization and design services, ensuring
+            your ideas are translated into robust hardware designs.
+          </Paragraph>
+        </CardContent>
+        <CardContent>
+          <Numberunfill>02</Numberunfill>
+          <SemiHeading>Prototyping</SemiHeading>
+          <Paragraph>
+            We utilize state-of-the-art prototyping techniques to create
+            physical models of your hardware concepts. Our rapid prototyping
+            services allow for quick iterations, ensuring your product evolves
+            efficiently.
+          </Paragraph>
+        </CardContent>
+        <CardContent>
+          <Numberunfill>03</Numberunfill>
+          <SemiHeading>PCB Design</SemiHeading>
+          <Paragraph>
+            Our skilled engineers design custom Printed Circuit Boards (PCBs)
+            tailored to your specifications. From schematic capture to layout
+            design, we ensure optimal performance and reliability.
+          </Paragraph>
+        </CardContent>
+        <CardContent>
+          <Numberunfill>04</Numberunfill>
+          <SemiHeading>Embedded Systems Development</SemiHeading>
+          <Paragraph>
+            We utilize state-of-the-art prototyping techniques to create
+            physical models of your hardware concepts. Our rapid prototyping
+            services allow for quick iterations, ensuring your product evolves
+            efficiently.
+          </Paragraph>
+        </CardContent>
+        <CardContent>
+          <Numberunfill>05</Numberunfill>
+          <SemiHeading>Testing and Quality Assurance</SemiHeading>
+          <Paragraph>
+            Rigorous testing procedures guarantee the functionality,
+            reliability, and durability of your hardware prototypes. We adhere
+            to the highest quality standards to deliver products that exceed
+            expectations.
+          </Paragraph>
+        </CardContent>
+        <CardContent>
+          <Numberunfill>06</Numberunfill>
+          <SemiHeading>Manufacturing Support</SemiHeading>
+          <Paragraph>
+            Beyond prototyping, we offer manufacturing support services,
+            assisting you in transitioning from prototype to mass production.
+            Our guidance ensures a smooth and cost-effective manufacturing
+            process.
+          </Paragraph>
+        </CardContent>
+      </FlexContent>
+    </div>
+  );
+}
