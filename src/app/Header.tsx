130f4c928420ab74bebc54ad6c8b97c007116414
abarnashiva <abarnaabi57@gmail.com>
2023-12-26 18:48:27 +0530

    add: changes in the header

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,51 +1,98 @@
-'use client';
-import React, { useState } from 'react';
-import { Arrow } from './common';
+"use client";
+import React, { useEffect, useState } from "react";
+import { Arrow } from "./common";
 import { FaBars } from "react-icons/fa6";
-import Link from 'next/link';
-
+import Link from "next/link";
 
 function Header() {
   const [click, setClick] = useState(false);
-  const [innerWidth, setInnerWidth] = useState(0);
-  React.useEffect(() => {
-    function handleResize() {
-setInnerWidth(window.innerWidth)
-}
-window.addEventListener('resize', handleResize)
-  }
-  )
+  const [smallScreen, setSmallScreen] = useState(true);
+
+  const handleResize = () => {
+    setSmallScreen(window.innerWidth <= 500);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <header className=" scroll-smooth flex justify-between py-6 px-6 bg-neutral-50 text-neutral-900 shadow-md">
-      <Link href="/"><img src="/praxio.svg" alt="Image Description" className="w-32" /></Link>
+      <Link href="/">
+        <img src="/praxio.svg" alt="Image Description" className="w-32" />
+      </Link>
       <nav>
-        {innerWidth < 630 && !click && <div className='text-red-600 right-0  w-8 text-center cursor-pointer text-2xl font-medium rounded'
-          onClick={()=>setClick(true)}>
-          {<FaBars/>}</div>}
-      <ul className={`${innerWidth < 630 && click ?`flex fixed p-4 w-full inset-y-0 right-0 bg-neutral-50 z-50 flex-col h-auto text-base overflow-hidden space-y-8 `:
-        innerWidth < 630 ? `hidden` :`flex space-x-4 md:space-x-8 `} `}>
-        <li className='navList px-7 md:px-3'><a href="#home" onClick={()=>setClick(false)}>Home</a></li>
-        <hr/>
-        <li className='navList px-7 md:px-3'><a href="#about" onClick={()=>setClick(false)}>About Us</a></li>
-        <hr/>
-        <li className='navList px-7 md:px-3'><a href="/service" onClick={()=>setClick(false)}>Service</a></li>
-        <hr/>
-        <li className='navList px-7 md:px-3'><a href="#career" onClick={()=>setClick(false)}>Career</a></li>
-        <hr/>
-        <li className='navList px-7 md:px-3'><a href="#contact" onClick={()=>setClick(false)}>Contact</a></li>
-        <hr/>
-        {innerWidth < 630 &&
-        <button className="group flex items-center shadow-sm shadow-neutral-500 px-5 py-3 bg-red-600 hover:bg-red-500
+        {smallScreen ? (
+          <div
+            className="text-red-600 right-0  w-8 text-center cursor-pointer text-2xl font-medium rounded"
+            onClick={() => setClick(true)}
+          >
+            {<FaBars />}
+          </div>
+        ) : (
+          ""
+        )}
+        <ul
+          className={`${
+            smallScreen && click
+              ? `flex fixed p-4 w-full inset-y-0 right-0 bg-neutral-50 z-50 flex-col h-auto text-base overflow-hidden space-y-8 `
+              : smallScreen
+              ? "hidden"
+              : "flex space-x-8 sm:space-x-4"
+          }  `}
+        >
+          <li className="navList px-3 md:px-1 sm:px-0.5">
+            <a href="/home" onClick={() => setClick(false)}>
+              Home
+            </a>
+          </li>
+          <hr />
+          <li className="navList px-3 md:px-1 sm:px-0.5">
+            <a href="/about" onClick={() => setClick(false)}>
+              About Us
+            </a>
+          </li>
+          <hr />
+          <li className="navList px-3 md:px-1 sm:px-0.5">
+            <a href="/about" onClick={() => setClick(false)}>
+              Products & Solutions
+            </a>
+          </li>
+          <hr />
+          <li className="navList px-3 md:px-1 sm:px0.5">
+            <a href="/service" onClick={() => setClick(false)}>
+              Services
+            </a>
+          </li>
+          <hr />
+          <li className="navList px-3 md:px-1 sm:px-0.5">
+            <a href="#career" onClick={() => setClick(false)}>
+              Contact
+            </a>
+          </li>
+          <hr />
+          <li className="navList px-3 md:px-1 sm:px-0.5">
+            <a href="#contact" onClick={() => setClick(false)}>
+              Support
+            </a>
+          </li>
+          <hr />
+          {smallScreen && (
+            <button
+              className="group flex items-center shadow-sm shadow-neutral-500 px-5 py-3 bg-red-600 hover:bg-red-500
           text-white transition-all"
-          onClick={()=>setClick(false)} >
-      <span className="group-hover:-translate-x-4 transition-transform rotate-180 w-4 mx-4">
-        <Arrow />
-      </span>
-      <span>Back</span>
-    </button>}
-      </ul>
-
+              onClick={() => setClick(false)}
+            >
+              <span className="group-hover:-translate-x-4 transition-transform rotate-180 w-4 mx-4">
+                <Arrow />
+              </span>
+              <span>Back</span>
+            </button>
+          )}
+        </ul>
       </nav>
     </header>
   );
