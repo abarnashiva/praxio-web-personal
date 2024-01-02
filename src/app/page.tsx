4fb771eebdb50098d080b8ddc4600516c2c0c8db
abarnashiva <abarnaabi57@gmail.com>
2024-01-02 19:07:44 +0530

    add: home page to the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { redirect } from "next/navigation";
+import Home from "./home/page";
 
 export default function Page() {
   redirect("/home");
 
-  return <p>No text</p>;
+  return <Home />;
 }
